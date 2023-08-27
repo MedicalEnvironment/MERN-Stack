f3d1eb1b0b4274bc909bed3908ad0972b5ceefbc
Akbar <akbar.abayev@gmail.com>
2023-08-27 11:34:34 +0600

    fetching data with an api and endpoints, GET and POST deck endpoints

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState('');
+
+  async function handleCreateDeck(e: React.FormEvent) 
+  {
+    e.preventDefault();
+    await fetch('http://localhost:5000/decks', {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    setTitle("");
+  }
+
+  useEffect(() => {
+    async function fetchDecks() {
+      await fetch("http://localhost:5000/decks");
+    }
+    fetchDecks();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <div className="App">
+    <form onSubmit={handleCreateDeck} >
+      <label htmlFor="deck-title">Deck Title</label>
+      <input
+       id="deck-title"
+       value={title} 
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value);
+          }}     
+        />
+        <button>Create Deck</button>
+      </form>
+  </div>
+  );
 }
 
-export default App
+export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,8 @@ import { config } from "dotenv";
 config();
 
 import express, { Request, Response } from "express";
-import mongoose from 'mongoose';
+import mongoose from "mongoose" ;
+import cors from "cors";
 
 import Deck from "./models/Deck";
 
@@ -10,7 +11,20 @@ const PORT = 5000;
 
 const app = express();
 
+app.use(cors(
+    {
+        origin: "*",
+    }
+));
 app.use(express.json());
+
+app.get('/decks', async (req: Request, res: Response) => {
+    //TODO: fetch all decks adn send em back to user
+    //1. how do we fetch the decks from mongo?
+    const decks = await Deck.find();
+    //2. how do we send back the array to the ui?
+    res.json(decks);
+})
 
 app.post("/decks", async (req: Request, res: Response) => {
     const newDeck = new Deck({
