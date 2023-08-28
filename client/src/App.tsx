9e1e25e13c98720856a6633679007a3bac24f3af
MedicalEnvironment <akbar.abayev@gmail.com>
2023-08-28 11:28:55 +0600

    optimizing code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import './App.css';
-
-type TDeck = {
-  title: string;
-  _id: string;
-}
+import { deleteDeck } from "./api/deleteDeck";
+import { TDeck, getDecks } from "./api/getDecks";
+import { createDeck } from "./api/createDeck";
 
 function App() {
   const [decks, setDecks] = useState<TDeck[]>([]);
@@ -13,32 +12,19 @@ function App() {
   async function handleCreateDeck(e: React.FormEvent) 
   {
     e.preventDefault();
-    const response = await fetch('http://localhost:5000/decks', {
-      method: "POST",
-      body: JSON.stringify({
-        title,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const deck = await response.json()
+    const deck = await createDeck(title)
     setDecks([...decks, deck])
     setTitle("");
   }
 
   async function handleDeleteDeck(deckId: string) {
-    await fetch(`http://localhost:5000/decks/${deckId}`, {
-      method: "DELETE",
-    });
-    //refetch all my todo items
+    await deleteDeck(deckId);
     setDecks(decks.filter((deck) => deck._id !== deckId));
   }
 
   useEffect(() => {
     async function fetchDecks() {
-      const response = await fetch("http://localhost:5000/decks");
-      const newDecks = await response.json();
+      const newDecks = await getDecks();
       setDecks(newDecks);
     }
     fetchDecks();
@@ -51,7 +37,8 @@ function App() {
         decks.map((deck) => (
           <li key={deck._id}>
             <button onClick={() => handleDeleteDeck(deck._id)}>X</button>
-            {deck.title}
+
+            <Link to={`decks/${deck._id}`}>{deck.title}</Link>
             </li>
         ))}
       </ul>
