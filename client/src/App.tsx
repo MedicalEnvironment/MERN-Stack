70e866f7a44a8e373aecdc45f3534918dcd7c40e
MedicalEnvironment <akbar.abayev@gmail.com>
2023-08-28 07:31:29 +0600

    showing the decks on the landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
 
+type TDeck = {
+  title: string;
+  _id: string;
+}
+
 function App() {
-  const [title, setTitle] = useState('');
+  const [decks, setDecks] = useState<TDeck[]>([]);
+  const [title, setTitle] = useState("");
 
   async function handleCreateDeck(e: React.FormEvent) 
   {
@@ -21,13 +27,21 @@ function App() {
 
   useEffect(() => {
     async function fetchDecks() {
-      await fetch("http://localhost:5000/decks");
+      const response = await fetch("http://localhost:5000/decks");
+      const newDecks = await response.json();
+      setDecks(newDecks);
     }
     fetchDecks();
   }, []);
 
   return (
   <div className="App">
+    <ul className="decks">
+      {
+        decks.map((deck) => (
+          <li key={deck._id}>{deck.title}</li>
+        ))}
+      </ul>
     <form onSubmit={handleCreateDeck} >
       <label htmlFor="deck-title">Deck Title</label>
       <input
